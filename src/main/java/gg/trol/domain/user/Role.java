3a7bfb5d722dbc8e608ef1e2e013288963b219dc
Jinsup9k <01077717141p@gmail.com>
2020-10-19 20:04:08 +0900

    connect jpa & add proto google login

---
+++ src/main/java/gg/trol/domain/user/Role.java
@@ -0,0 +1,15 @@
+package gg.trol.domain.user;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Role {
+    GUEST("ROLE_GUEST", "손님"),
+    USER("ROLE_USER", "일반 사용자"),
+    ADMIN("ROLE_USER", "운영자");
+
+    private final String key;
+    private final String title;
+}
---
+++ src/main/java/gg/trol/domain/user/UserRepository.java
@@ -0,0 +1,9 @@
+package gg.trol.domain.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
