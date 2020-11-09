d0974148b654f71d64a8a56f611b184d8f03c353
Jinsup9k <01077717141p@gmail.com>
2020-11-09 18:31:38 +0900

    modi security config

--- src/main/java/gg/trol/controller/v1/SignController.java
+++ src/main/java/gg/trol/controller/v1/SignController.java
@@ -30,37 +30,10 @@ public class SignController {
     private final ResponseService responseService;
     private final KakaoService kakaoService;
 
-    /*    @ApiOperation(value = "로그인", notes = "이메일 회원 로그인을 한다.")
-        @PostMapping(value = "/signin")
-        public SingleResult<String> signin(@ApiParam(value = "회원ID : 이메일", required = true) @RequestParam String id,
-                                           @ApiParam(value = "비밀번호", required = true) @RequestParam String password) {
-            User user = userRepository.findByUid(id).orElseThrow(CEmailSigninFailedException::new);
-            if (!passwordEncoder.matches(password, user.getPassword()))
-                throw new CEmailSigninFailedException();
-
-            return responseService.getSingleResult(jwtTokenProvider.createToken(String.valueOf(user.getMsrl()), user.getRoles()));
-
-        }
-
-        @ApiOperation(value = "가입", notes = "회원가입을 한다.")
-        @PostMapping(value = "/signup")
-        public CommonResult signin(@ApiParam(value = "회원ID : 이메일", required = true) @RequestParam String id,
-                                   @ApiParam(value = "비밀번호", required = true) @RequestParam String password,
-                                   @ApiParam(value = "이름", required = true) @RequestParam String name) {
-
-            userJpaRepo.save(User.builder()
-                    .uid(id)
-                    .password(passwordEncoder.encode(password))
-                    .name(name)
-                    .roles(Collections.singletonList("ROLE_USER"))
-                    .build());
-            return responseService.getSuccessResult();
-        }
-        */
     @ApiOperation(value = "소셜 로그인", notes = "소셜 회원 로그인을 한다.")
     @PostMapping(value = "/signin/{provider}")
     public SingleResult<String> signinByProvider(
-            @ApiParam(value = "서비스 제공자 provider", required = true, defaultValue = "kakao") @PathVariable String provider,
+            @ApiParam(value = "서비스 제공자 provider", required = true) @PathVariable String provider,
             @ApiParam(value = "소셜 access_token", required = true) @RequestParam String accessToken) {
 
         KakaoProfile profile = kakaoService.getKakaoProfile(accessToken);
@@ -70,7 +43,7 @@ public class SignController {
 
     @ApiOperation(value = "소셜 계정 가입", notes = "소셜 계정 회원가입을 한다.")
     @PostMapping(value = "/signup/{provider}")
-    public CommonResult signupProvider(@ApiParam(value = "서비스 제공자 provider", required = true, defaultValue = "kakao") @PathVariable String provider,
+    public CommonResult signupProvider(@ApiParam(value = "서비스 제공자 provider", required = true) @PathVariable String provider,
                                        @ApiParam(value = "소셜 access_token", required = true) @RequestParam String accessToken,
                                        @ApiParam(value = "이름", required = true) @RequestParam String name) {
 
--- src/main/java/gg/trol/service/user/CustomOAuth2UserService.java
+++ src/main/java/gg/trol/service/user/CustomOAuth2UserService.java
@@ -1,70 +1,99 @@
 package gg.trol.service.user;
 
-import com.google.gson.Gson;
-import gg.trol.advice.exception.CCommunicationException;
-import gg.trol.model.user.KakaoProfile;
-import gg.trol.model.user.RetKakaoAuth;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.*;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.http.OAuth2ErrorResponseErrorHandler;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequestEntityConverter;
 import org.springframework.security.oauth2.core.OAuth2AccessToken;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.OAuth2AuthorizationException;
+import org.springframework.security.oauth2.core.OAuth2Error;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
+import org.springframework.security.oauth2.core.user.OAuth2UserAuthority;
+import org.springframework.util.Assert;
 import org.springframework.util.StringUtils;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestOperations;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 import java.util.Map;
-import java.util.Objects;
+import java.util.Set;
 
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
-
-    private RestTemplate restTemplate;
-    private Gson gson;
-
+    private static final String MISSING_USER_INFO_URI_ERROR_CODE = "missing_user_info_uri";
+    private static final String MISSING_USER_NAME_ATTRIBUTE_ERROR_CODE = "missing_user_name_attribute";
+    private static final String INVALID_USER_INFO_RESPONSE_ERROR_CODE = "invalid_user_info_response";
     private static final ParameterizedTypeReference<Map<String, Object>> PARAMETERIZED_RESPONSE_TYPE = new ParameterizedTypeReference<Map<String, Object>>() {
     };
+    private Converter<OAuth2UserRequest, RequestEntity<?>> requestEntityConverter = new OAuth2UserRequestEntityConverter();
+    private RestOperations restOperations;
 
-    public OAuth2User loadUser(OAuth2UserRequest userRequest) {
-        // Set header : Content-type: application/x-www-form-urlencoded
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-        headers.set("Authorization", "Bearer " + userRequest);
+    public CustomOAuth2UserService() {
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.setErrorHandler(new OAuth2ErrorResponseErrorHandler());
+        this.restOperations = restTemplate;
+    }
 
-        // Set http entity
-        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(null, headers);
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        Assert.notNull(userRequest, "userRequest cannot be null");
+        if (!StringUtils.hasText(userRequest.getClientRegistration().getProviderDetails().getUserInfoEndpoint().getUri())) {
+            OAuth2Error oauth2Error = new OAuth2Error(MISSING_USER_INFO_URI_ERROR_CODE, "Missing required UserInfo Uri in UserInfoEndpoint for Client Registration: " + userRequest.getClientRegistration().getRegistrationId(), null);
+            throw new OAuth2AuthenticationException(oauth2Error, oauth2Error.toString());
+        }
+        String userNameAttributeName = userRequest.getClientRegistration().getProviderDetails().getUserInfoEndpoint().getUserNameAttributeName();
+        if (!StringUtils.hasText(userNameAttributeName)) {
+            OAuth2Error oauth2Error = new OAuth2Error(MISSING_USER_NAME_ATTRIBUTE_ERROR_CODE, "Missing required \"user name\" attribute name in UserInfoEndpoint for Client Registration: " + userRequest.getClientRegistration().getRegistrationId(), null);
+            throw new OAuth2AuthenticationException(oauth2Error, oauth2Error.toString());
+        }
+        RequestEntity<?> request = this.requestEntityConverter.convert(userRequest);
+        ResponseEntity<Map<String, Object>> response;
         try {
-            // Request profile
-            ResponseEntity<String> response = restTemplate.postForEntity(userRequest.getClientRegistration().getProviderDetails().getUserInfoEndpoint().getUri(), request, String.class);
-            if (response.getStatusCode() == HttpStatus.OK)
-                return gson.fromJson(response.getBody(), OAuth2User.class);
-        } catch (Exception e) {
-            throw new CCommunicationException();
+            response = this.restOperations.exchange(request, PARAMETERIZED_RESPONSE_TYPE);
+        } catch (OAuth2AuthorizationException ex) {
+            OAuth2Error oauth2Error = ex.getError();
+            StringBuilder errorDetails = new StringBuilder();
+            errorDetails.append("Error details: [");
+            errorDetails.append("UserInfo Uri: ").append(userRequest.getClientRegistration().getProviderDetails().getUserInfoEndpoint().getUri());
+            errorDetails.append(", Error Code: ").append(oauth2Error.getErrorCode());
+            if (oauth2Error.getDescription() != null) {
+                errorDetails.append(", Error Description: ").append(oauth2Error.getDescription());
+            }
+            errorDetails.append("]");
+            oauth2Error = new OAuth2Error(INVALID_USER_INFO_RESPONSE_ERROR_CODE, "An error occurred while attempting to retrieve the UserInfo Resource: " + errorDetails.toString(), null);
+            throw new OAuth2AuthenticationException(oauth2Error, oauth2Error.toString(), ex);
+        } catch (RestClientException ex) {
+            OAuth2Error oauth2Error = new OAuth2Error(INVALID_USER_INFO_RESPONSE_ERROR_CODE, "An error occurred while attempting to retrieve the UserInfo Resource: " + ex.getMessage(), null);
+            throw new OAuth2AuthenticationException(oauth2Error, oauth2Error.toString(), ex);
         }
-        throw new CCommunicationException();
-
-
+        Map<String, Object> userAttributes = getUserAttributes(response);
+        Set<GrantedAuthority> authorities = new LinkedHashSet<>();
+        authorities.add(new OAuth2UserAuthority(userAttributes));
+        OAuth2AccessToken token = userRequest.getAccessToken();
+        for (String authority : token.getScopes()) {
+            authorities.add(new SimpleGrantedAuthority("SCOPE_" + authority));
+        }
+        return new DefaultOAuth2User(authorities, userAttributes, userNameAttributeName);
     }
 
-    public RetKakaoAuth getTokenInfo(String code) {
-        // Set header : Content-type: application/x-www-form-urlencoded
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-        // Set parameter
-        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-        params.add("grant_type", "authorization_code");
-        params.add("client_id", );
-        params.add("redirect_uri", baseUrl + redirect);
-        params.add("code", code);
-        // Set http entity
-        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(params, headers);
-        ResponseEntity<String> response = restTemplate.postForEntity(Objects.requireNonNull(env.getProperty("spring.social.kakao.url.token")), request, String.class);
-        if (response.getStatusCode() == HttpStatus.OK) {
-            return gson.fromJson(response.getBody(), RetKakaoAuth.class);
+    // 네이버는 HTTP response body에 response 안에 id 값을 포함한 유저정보를 넣어주므로 유저정보를 빼내기 위한 작업을 함
+    private Map<String, Object> getUserAttributes(ResponseEntity<Map<String, Object>> response) {
+        Map<String, Object> userAttributes = response.getBody();
+        if (userAttributes.containsKey("response")) {
+            LinkedHashMap responseData = (LinkedHashMap) userAttributes.get("response");
+            userAttributes.putAll(responseData);
+            userAttributes.remove("response");
         }
-        return null;
+        return userAttributes;
     }
-
 }
+
