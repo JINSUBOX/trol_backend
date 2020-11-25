39eb5c7c9d782f7b01a7b8b7f650af6d7d7c68b4
Jinsup9k <01077717141p@gmail.com>
2020-11-25 21:58:21 +0900

    security dummy

---
+++ src/main/java/gg/trol/config/security/dummy.java
@@ -0,0 +1,10 @@
+package gg.trol.config.security;
+
+public class dummy {
+
+    public void dummyData(){
+        String dummyData = "this is dummy data";
+        String test = "this is test String";
+
+    }
+}
