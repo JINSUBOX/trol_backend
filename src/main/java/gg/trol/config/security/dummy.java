cbdf59176544cc7198c1a656af70fc2bf2b0902c
Jinsup9k <01077717141p@gmail.com>
2020-11-27 21:45:05 +0900

    data modified

--- src/main/java/gg/trol/config/security/dummy.java
+++ src/main/java/gg/trol/config/security/dummy.java
@@ -5,6 +5,6 @@ public class dummy {
     public void dummyData(){
         String dummyData = "this is dummy data";
         String test = "this is test String";
-
+        String dummyData2 = "today is awesome";
     }
 }
